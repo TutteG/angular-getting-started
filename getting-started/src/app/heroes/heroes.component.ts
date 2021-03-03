576984b58b99e86590bbf69307e5cec502105362
Torben Grove <torben_grove@live.dk>
2021-03-03 13:41:20 +0100

    Get data from server
    I added the necessary dependencies to use HTTP in the app.
    I refactored HeroService to load heroes from a web API.
    I extended HeroService to support post(), put(), and delete() methods.
    I updated the components to allow adding, editing, and deleting of heroes.
    I configured an in-memory web API.
    I learned how to use observables.

--- getting-started/src/app/heroes/heroes.component.ts
+++ getting-started/src/app/heroes/heroes.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
-import { Hero } from "../hero"
-import { HeroService } from "../hero.service"
-import { MessageService } from "../message.service"
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -20,6 +21,20 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => this.heroes.push(hero))
+  }
+
+  delete(hero: Hero): void{
+    this.heroes = this.heroes.filter(h => h !== hero)
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
 
